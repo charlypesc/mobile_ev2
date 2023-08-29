96a1706a9226997ff99c4f0f48cbcb38bf3d6b20
charlypesc <cparedes.escobar@gmail.com>
2023-08-29 18:04:41 -0400

    :construction: se agrega navegabilidad

--- app/src/main/java/com/example/listatareas/MainActivity.kt
+++ app/src/main/java/com/example/listatareas/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.example.listatareas
 
+import android.content.Intent
 import android.graphics.drawable.Icon
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -17,6 +20,7 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.CheckCircle
 import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -26,6 +30,7 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
@@ -42,106 +47,32 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-         lifecycleScope.launch(Dispatchers.IO) {
-             val tareaDao       = AppDataBase.getInstance(this@MainActivity).tareaDao()
-             val cantRegistros  = tareaDao.contar()
 
-             if(cantRegistros < 1){
-                 tareaDao.insertar(Tarea(0, "Leche", false))
-                 tareaDao.insertar(Tarea(0, "Choclos", false))
-             }
-         }
         setContent {
-            ListaTareasUI()
+            PantallaInicio()
         }
     }
+
+    @Preview
     @Composable
-    fun ListaTareasUI(){
+    fun PantallaInicio() {
         val contexto = LocalContext.current
-        val (tareas, setTareas)= remember{ mutableStateOf(emptyList<Tarea>()) }
-        LaunchedEffect(tareas){
-            withContext(Dispatchers.IO){
-                val dao = AppDataBase.getInstance(contexto).tareaDao()
-                setTareas(dao.getAll())
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            Button(onClick = {
+                val intento = Intent(contexto, ListadoActivity::class.java)
+                contexto.startActivity(intento)
+            }) {
+                Text("Listado de Compras")
             }
-        }
-        LazyColumn(
-            modifier = Modifier.fillMaxSize()
-        ){
-            items(tareas){tarea ->
-                TareaItemUI(tarea){
-                    setTareas(emptyList<Tarea>())
-                }
+            Button(onClick = {
+
+            }) {
+                Text("Crear")
             }
         }
     }
-}
-@Composable
-fun TareaItemUI(tarea:Tarea, onSave:() -> Unit = {}){
-    val contexto = LocalContext.current
-    val alcanceCorrutina = rememberCoroutineScope()
-    Row(
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(vertical = 20.dp, horizontal = 20.dp)
-    ){
-        if(tarea.realizada){
-            Icon(
-                Icons.Filled.CheckCircle,
-                contentDescription = "Tarea realizada",
-                modifier = Modifier.clickable {
-                    alcanceCorrutina.launch(Dispatchers.IO ){
-                        val dao = AppDataBase.getInstance( contexto).tareaDao()
-                        tarea.realizada=false
-                        dao.actualizar(tarea)
-                        onSave()
-                    }
-                }
-            )
-        }else{
-            Icon(
-                Icons.Filled.Add,
-                contentDescription = "Tarea pendiente",
-                modifier = Modifier.clickable {
-                    alcanceCorrutina.launch(Dispatchers.IO ){
-                        val dao = AppDataBase.getInstance( contexto).tareaDao()
-                        tarea.realizada=true
-                        dao.actualizar(tarea)
-                        onSave()
-                    }
-                }
-            )
-        }
-        Spacer(modifier = Modifier.width(20.dp))
-        Text(
-            text = tarea.tarea,
-            modifier = Modifier.weight(2f)
-        )
-
-        Icon(
-            Icons.Filled.Delete,
-            contentDescription = "Eliminar tarea",
-            modifier = Modifier.clickable {
-                alcanceCorrutina.launch(Dispatchers.IO ){
-                    val dao = AppDataBase.getInstance( contexto).tareaDao()
-                    dao.eliminar(tarea)
-                    onSave()
-                }
-            }
-        )
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun TareaItemUIPreview(){
-    val tarea = Tarea(1, "azucar", false)
-    TareaItemUI(tarea)
-}
-
-@Preview(showBackground = true)
-@Composable
-fun TareaItemUIPreview2(){
-    val tarea = Tarea(2, "platanos", true)
-    TareaItemUI(tarea)
 }
